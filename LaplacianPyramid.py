7fda8eeb03c39fa584c73e7c7eb6c5d9b2d3591b
nhunghoang <nhoang4673@gmail.com>
2017-02-21 21:50:49 -0500

    add reconstruct code and fix bear size

--- LaplacianPyramid.py
+++ LaplacianPyramid.py
@@ -14,8 +14,7 @@ def pyr_build(img, N=6):
     for i in range(N):
         if i == N-1:
             # If we're at the smallest size pyramid image
-            lp.append(G_i)
-            L = lp[-1]
+            break
             # cv2.imshow('cvland', 0.5 + 0.5 *(L / np.abs(L).max()))
             # cv2.waitKey(0)
         else:
@@ -29,15 +28,33 @@ def pyr_build(img, N=6):
             # cv2.imshow( 'cvland', 0.5 + 0.5 *(L / np.abs(L).max()))
             # cv2.waitKey(0)
             # print 'shape G_i_plus', G_i_plus.shape
+    lp.append(G_i)
+    L = lp[-1]
+    return lp
 
+def pyr_reconstruct(lp):
+    Rn = lp[-1]
+    for i in reversed(lp[0:-1]):
+        R_i_up = np.zeros(i.shape)
+        R_i_up = cv2.pyrUp(Rn, R_i_up)
+        R_i_minus = R_i_up + i
+        Rn = R_i_minus.copy() 
+    #cv2.imshow('cvland', np.array(R_i_minus, dtype=np.uint8))
+    #cv2.waitKey(0)
+    return R_i_minus
 
 def main():
 
 
-    image = "Images/penguin.png"
-    base_image = cv2.imread(image)
-    pyr_build(base_image)
+    image1 = "Images/penguin.png"
+    base_image1 = cv2.imread(image1)
+    lp1 = pyr_build(base_image1)
+    reconstructed1 = pyr_reconstruct(lp1)
 
+    image2 = "Images/bearface.jpg"
+    base_image2 = cv2.imread(image2)
+    lp2 = pyr_build(base_image2)
+    reconstructed2 = pyr_reconstruct(lp2)
 
 if __name__=='__main__':
     main()
