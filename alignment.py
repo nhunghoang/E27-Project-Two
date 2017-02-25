ce6268c311b7b77be67ea5dcfb1dda37df76cc63
nhunghoang <nhoang4673@gmail.com>
2017-02-24 22:59:20 -0500

    add alignment program

---
+++ alignment.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+import cvk2
+
+def main():
+	imgA = cv2.imread("Images/sunflower.jpg")
+	imgB = cv2.imread("Images/bear34.jpeg")
+	height, width, ret = imgA.shape
+	'''cv2.imshow("Image A", imgA)
+	cv2.waitKey(0)	
+	bwA = cv2.cvtColor(imgA, cv2.COLOR_BGR2GRAY)
+	bwB = cv2.cvtColor(imgB, cv2.COLOR_BGR2GRAY)
+	cv2.imshow("Image A BW", bwA)
+	cv2.waitKey(0)'''	
+
+	# grayscale based on petal color
+	yellow = (0, 220, 248)
+	diffs_per_channel = imgA.astype(np.float32) - yellow
+	squared_dists = (diffs_per_channel**2).sum(axis=2)
+	dists = np.sqrt(squared_dists)
+	bwA = np.clip(dists, 0, 255).astype(np.uint8)
+	'''cv2.imshow("Image A BW", bwA)
+	cv2.waitKey(0)'''	
+	
+	#threshold and invert
+	#clear speckles off background 
+	ret, threshA = cv2.threshold(bwA, 125, 255, cv2.THRESH_BINARY)
+	threshA = 255-threshA
+	'''cv2.imshow("Image A THRES", threshA)
+	cv2.waitKey(0)'''	
+	morphA = cv2.morphologyEx(threshA, cv2.MORPH_ERODE, np.ones((3,3),np.uint8))
+	cv2.imshow("Image A MORPH", morphA)
+	cv2.waitKey(0)
+
+	#find the object as a contour and bound it by rectangle
+	cpA = morphA.copy()
+	contourImgA = np.zeros((cpA.shape[0], cpA.shape[1], 3), dtype='uint8')
+	image, contours, hierarchy = cv2.findContours(cpA, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+	contours = sorted(contours, key=cv2.contourArea)
+	x,y,w,h = cv2.boundingRect(contours[-1])
+	centered = np.zeros_like(imgA)
+	rect = cv2.rectangle(contourImgA, (x,y), (x+w,y+h), (135,65,86))
+	cv2.drawContours(contourImgA, contours, -1, (44,134,88), -1 )
+	cv2.imshow("Image A CONT", contourImgA)
+	cv2.waitKey(0)
+	#shifting to center
+	contour_half_width = (w-x)/2
+	contour_half_height = (h-y)/2
+	center_top = (height/2)-contour_half_height
+	center_bottom = center_top+h
+	center_left = (width/2)-contour_half_width
+	center_right = center_left+w
+	accum = 0
+	for row in range(center_top, center_bottom+1):
+		centered[row] = contourImgA[accum]
+		accum += 1
+	cv2.imshow("Shifted down",centered)
+	cv2.waitKey(0)
+	shift = x-center_left
+	for col in range(center_left, center_right+1):
+		for row in range(center_top, center_bottom+1):
+			centered[row][col] = centered[row][col+shift]
+	cv2.imshow("Shifted left",centered)
+	cv2.waitKey(0)
+
+
+
+main()
