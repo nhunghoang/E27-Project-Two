be511c1f59f66b32bd590deae76e598349fdd40c
th14dan <th14dan@gmail.com>
2017-02-26 18:16:47 -0500

    Add files via upload
    i think this is a good set of params

--- hybrid_image.py
+++ hybrid_image.py
@@ -8,38 +8,57 @@ import sys
 import numpy as np
 import cv2
 
-def lopass(img, sigma):
+def lopass(img, sigma, ksize):
+    lofil = cv2.GaussianBlur(img, (ksize,ksize), sigma) 
+    return lofil
     
-    return cv2.GaussianBlur(img, (7,7), sigma)
-    
-def hipass(img, sigma):
-    return img - lopass(img, sigma)
+def hipass(img, sigma, ksize):
+    lofil = lopass(img, sigma, ksize)
+    hifil = img-lofil
+    """
+    hifil -= hifil.min()
+    cv2.imshow("win", hifil.astype(np.uint8))
+    cv2.waitKey(0)
+    """
+    return hifil
     
 def main():
     if len(sys.argv) != 3:
         print("usage: img_pyr_blend.py img1 img2")
         exit(0)
+
+    # read images from input files        
+    imgA = (cv2.imread(sys.argv[1])).astype(np.float32)
+    imgB = (cv2.imread(sys.argv[2])).astype(np.float32)
+    # convert to greyscale
+    g_imgA = cv2.cvtColor(imgA, cv2.COLOR_BGR2GRAY)
+    g_imgB = cv2.cvtColor(imgB, cv2.COLOR_BGR2GRAY)
     
-    imgA = cv2.imread(sys.argv[1])
-    imgB = cv2.imread(sys.argv[2])
+    # set parameters:
+    # kA & kB are the weights of each image when added
+    kA = 1
+    kB = 1
+    # sigA and sigB are the stdev values for the Gaussian blur
+    sigA = 5
+    sigB = 3
     
-    kA = 3
-    kB = 4
-    
-    sigA = 3
-    sigB = 2
-
-    lopassed = np.array(lopass(imgA, sigA)).astype(np.float32)
-    hipassed = np.array(hipass(imgB, sigB)).astype(np.float32)
-
-    hybrid = kA * lopassed + kB * hipassed
-
-    hybrid_norm = (hybrid *(255/hybrid.max())).astype(np.uint8)
-
     cv2.namedWindow("win")
-    cv2.imshow("win", hybrid_norm)
-    cv2.waitKey(0)
+
+    for i in range(1,24,2):
+        print i
+        lopassed = lopass(g_imgA, sigA, 9)
+        hipassed = hipass(g_imgB, sigB, 9)
+            
+        hybrid = kA * lopassed + kB * hipassed
+        
+        if hybrid.min() < 0:
+            hybrid -= hybrid.min()
+                
+        hybrid_norm = (hybrid *(255/hybrid.max())).astype(np.uint8)
     
+        cv2.imshow("win", hybrid_norm)
+        cv2.waitKey(0)
+
     cv2.imwrite("hybrid.jpg",hybrid_norm)
 
 main()
