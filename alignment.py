9780b26885d3442b5d9ac6c4a47549dbb3115972
nhunghoang <nhoang4673@gmail.com>
2017-02-25 23:51:31 -0500

    add centered images

--- alignment.py
+++ alignment.py
@@ -4,13 +4,7 @@ import cvk2
 
 def main():
 	imgA = cv2.imread("Images/sunflower.jpg")
-	imgB = cv2.imread("Images/bear34.jpeg")
-	'''cv2.imshow("Image A", imgA)
-	cv2.waitKey(0)	
-	bwA = cv2.cvtColor(imgA, cv2.COLOR_BGR2GRAY)
-	bwB = cv2.cvtColor(imgB, cv2.COLOR_BGR2GRAY)
-	cv2.imshow("Image A BW", bwA)
-	cv2.waitKey(0)'''	
+	imgB = cv2.imread("Images/zucker.jpeg")
 
 	# grayscale based on petal color
 	yellow = (0, 220, 248)
@@ -26,10 +20,6 @@ def main():
 	isolated = np.zeros_like(imgA)
 	mask = morphA.view(np.bool)
 	isolated[mask] = imgA[mask]
-	cv2.imshow("Image A MORPH", morphA)
-	cv2.waitKey(0)
-	cv2.imshow("Image A MORPH", isolated)
-	cv2.waitKey(0)
 
 	#find the object as a contour and bound it by rectangle
 	cpA = morphA.copy()
@@ -41,10 +31,8 @@ def main():
 	height,width, ret = centered.shape
 	rect = cv2.rectangle(contourImgA, (x,y), (x+w,y+h), (135,65,86))
 	cv2.drawContours(contourImgA, contours, -1, (44,134,88), -1 )
-	cv2.imshow("Image A CONT", contourImgA)
-	cv2.waitKey(0)
 
-	#shift object to center
+	#shift flower to center
 	center_top = (height/2)-(h/2)
 	center_bottom = center_top+h
 	center_left = (width/2)-(w/2)
@@ -53,8 +41,6 @@ def main():
 	for row in range(center_top, center_bottom+1):
 		centered[row] = isolated[accum]
 		accum += 1
-	cv2.imshow("Shifted down",centered)
-	cv2.waitKey(0)
 	shift = x-center_left
 	for col in range(center_left, center_right+1):
 		for row in range(center_top, center_bottom+1):
@@ -62,9 +48,47 @@ def main():
 	for col in range(center_right+1, width):
 		for row in range(center_top,center_bottom+1):
 			centered[row][col] = (0,0,0)
-	centered[
-	cv2.imshow("Shifted left",centered)
+	cv2.imwrite("centered_flower.jpg",centered)
+
+	#face recognition
+	#using tutorial: https://realpython.com/blog/python/face-recognition-with-python/
+	faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+	bwB = cv2.cvtColor(imgB, cv2.COLOR_BGR2GRAY)
+	faces = faceCascade.detectMultiScale(
+    			bwB,
+    			scaleFactor=1.1,
+    			minNeighbors=5,
+    			minSize=(15, 30),)
+	for (x,y,w,h) in faces:
+		cv2.rectangle(imgB, (x+50,y), (x+w,y+h), (0,0,0))
+	cv2.imshow("detect face", imgB)
 	cv2.waitKey(0)
 
-	#next to do: change black background to (60, 137, 100)
+	#generate face mask
+	(x,y,w,h) = faces[0]
+	mask = np.zeros_like(imgA)
+	for row in range(y,y+h+1):
+		for col in range(x+50,x+w+1):
+			mask[row][col] = imgB[row][col]
+
+	#shift face to center
+	center_top = (height/2)-(h/2)
+	center_bottom = center_top+h
+	center_left = (width/2)-((w-50)/2)
+	center_right = center_left+w
+	shiftUD = y-center_top
+	for row in range(center_top, center_bottom+1):
+		mask[row] = mask[row+shiftUD]
+	shiftLR = center_left-(x+50)
+	maskcp = mask.copy()
+	for col in range(center_left, center_right+1):
+		for row in range(center_top, center_bottom+1):
+			maskcp[row][col] = mask[row][col-shiftLR]
+	for col in range(width):
+		for row in range(center_bottom,height):
+			maskcp[row][col] = (0,0,0)
+	for col in range(0,center_left):
+		for row in range(0,center_bottom):
+			maskcp[row][col] = (0,0,0)
+	cv2.imwrite("centered_face.jpg",maskcp)
 main()
